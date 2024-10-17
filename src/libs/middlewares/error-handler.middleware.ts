246735a96777dd14b4c8d193d86275aa537b71d7
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-10-17 12:57:04 +0300

    * updated error handler for handling ApiError

--- src/libs/middlewares/error-handler.middleware.ts
+++ src/libs/middlewares/error-handler.middleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { ErrorMessage } from '../enums/error-message.enum';
 import { CustomError } from '../exceptions/http.exceptions';
 import { HTTPStatusCode } from '../enums/http-status-codes.enum';
+import { ApiError } from '../exceptions/api-error.exception';
 
 const exceptionHandler = (
   error: Error,
@@ -9,12 +10,12 @@ const exceptionHandler = (
   res: Response,
   _next: NextFunction
 ) => {
-
-  if(error instanceof CustomError){
+  if(error instanceof ApiError){
     return res
       .status(error.errorCode)
       .send(error.serializeErrors());
   }
+  /// Functionality to send a message about critical error
   return res
   .status(HTTPStatusCode.InternalServerError)
   .send({message: ErrorMessage.Generic});
