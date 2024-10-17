4fa197ca89c1d650d833e77511c6d451411e9248
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-10-17 13:44:51 +0300

    + testing route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { exceptionHandler } from './libs/middlewares/error-handler.middleware';
+import { componentRouter } from './components/components';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(componentRouter)
 
 app.use(exceptionHandler)
 
