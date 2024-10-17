f61df163ed7dbec2123c8c9b56b6333a20c04828
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-10-17 13:30:20 +0300

    * correct exports of errors

--- src/libs/exceptions/exceptions.ts
+++ src/libs/exceptions/exceptions.ts
@@ -1,7 +1,14 @@
 export { CustomError } from './custom-error.exception'
 export { ApiError } from './api-error.exception' 
 export { DatabaseConnectionError } from './database-connection-error.exception'
-export { ValidationError } from './api-errors/validation-error.exception'
+
+export { BadRequestError,
+    ConflictError,
+    NotFoundError,
+    UnauthorizedError,
+    UnprocessableEntityError,
+    ValidationError
+} from './api-errors/api-errors'
 
 export { ErrorMessage,
     ErrorType,
